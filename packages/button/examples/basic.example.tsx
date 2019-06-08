a9042b24d0c68f45a248d37862a5531523fe96f6
Sudhir Mitharwal <sudhir.mitharwal@tourepedia.com>
2019-06-08 18:09:09 +0530

    docs(button): add disable state example

--- packages/button/examples/basic.example.tsx
+++ packages/button/examples/basic.example.tsx
@@ -11,3 +11,11 @@ stories.add("Buttons", () => (
     <Button primary>Primary</Button>
   </div>
 ))
+stories.add("Disabled Buttons", () => (
+  <div>
+    <Button disabled>Default</Button>
+    <Button disabled primary>
+      Primary
+    </Button>
+  </div>
+))
