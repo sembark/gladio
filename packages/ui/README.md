a492db56186a7d3f1d416b59d00d5207b3928f90
Sudhir Mitharwal <sudhir.mitharwal@tourepedia.com>
2019-06-08 20:09:10 +0530

    feat(ui): combine all packages in one
    new package @tourepedia/ui that merges all the components and helpers

---
+++ packages/ui/README.md
@@ -0,0 +1,36 @@
+# UI
+
+All the Components and Helpers in one package
+
+## Installation
+
+```
+npm install --save @tourepedia/ui
+```
+
+## Usage
+
+### Add the styles
+
+in `SASS` or `SCSS`
+
+```scss
+@import "@tourepedia/ui/styles/index";
+```
+
+in `js`
+
+```js
+@import "@tourepedia/ui/styles/index.css";
+```
+
+### Using Components
+
+This is a combined package for all the ui elements so works the same way as others
+
+```js
+// prefered
+import { Button } from @tourepedia/ui
+// or
+import Button from "@tourepedia/button"
+```
---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,36 @@
+import Badge from "@tourepedia/badge"
+import Button from "@tourepedia/button"
+import Dialog, { useDialog } from "@tourepedia/dialog"
+import * as domHelpers from "@tourepedia/dom-helpers"
+import * as hooks from "@tourepedia/react-hooks"
+import {
+  Input,
+  Select as SelectInput,
+  TextArea,
+  FormGroup,
+  InputGroup,
+  InputGroupAddon,
+  ErrorMessage,
+} from "@tourepedia/input"
+import * as Icons from "@tourepedia/icons"
+
+import Select, { Async as AsyncSelect } from "@tourepedia/select"
+
+export {
+  Badge,
+  Button,
+  Dialog,
+  useDialog,
+  domHelpers,
+  hooks,
+  Input,
+  SelectInput,
+  TextArea,
+  FormGroup,
+  InputGroup,
+  InputGroupAddon,
+  ErrorMessage,
+  Select,
+  AsyncSelect,
+  Icons,
+}
