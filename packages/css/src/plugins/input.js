a0b4a0e7cbad4adf78ee0e0a8b8d5e9f57f9026c
Sudhir Mitharwal <sudhir.mitharwal@tourepedia.com>
2019-06-08 18:17:59 +0530

    feat(css): add disable states to input elements

--- packages/css/src/plugins/input.js
+++ packages/css/src/plugins/input.js
@@ -9,6 +9,7 @@ module.exports = function addInputComponentPlugin({ addComponents, theme }) {
       )}`,
       display: "block",
       lineHeight: 1,
+      maxWidth: "100%",
       "&:focus": {
         outline: "none",
         borderColor: theme("borderColor.primary.400"),
@@ -36,6 +37,17 @@ module.exports = function addInputComponentPlugin({ addComponents, theme }) {
       },
       "select&": {
         height: `calc(2 * ${theme("padding.ie-y")} + 1rem + 4px)`,
+      },
+      "&.has-error": {
+        borderColor: theme("borderColor.red.700"),
+      },
+      "&:disabled, &[readonly]": {
+        background: theme("backgroundColor.gray.100"),
+        cursor: "not-allowed",
+        opacity: 0.8,
+      },
+      "&[readonly]": {
+        pointerEvents: "none",
       },
     },
     ".input-group": {
@@ -67,9 +79,12 @@ module.exports = function addInputComponentPlugin({ addComponents, theme }) {
           "borderColor.gray.300"
         )}`,
       },
+      "&.has-error > *": {
+        borderColor: "red",
+      },
     },
     ".form-group": {
-      marginBottom: theme("margin.4"),
+      marginBottom: theme("margin.5"),
       "& > label": {
         fontSize: theme("fontSize.sm"),
       },
@@ -78,6 +93,19 @@ module.exports = function addInputComponentPlugin({ addComponents, theme }) {
       },
       "& > .input + label, & > .input-group + label": {
         marginLeft: theme("margin.2"),
+      },
+      "&.has-error .input, &.has-error .input-group > *": {
+        borderColor: theme("borderColor.red.700"),
+      },
+      "&.has-error .error-message": {
+        display: "block",
+        width: "fit-content",
+        backgroundColor: theme("backgroundColor.red.700"),
+        color: theme("textColor.white"),
+        marginTop: theme("margin.2"),
+        borderRadius: theme("borderRadius.default"),
+        fontSize: theme("fontSize.sm"),
+        padding: `${theme("padding.1")} ${theme("padding.3")}`,
       },
     },
   }
