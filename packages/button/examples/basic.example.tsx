d7e463c0b53eaf6a7af46c86d8cf09c45414e9a2
Sudhir Mitharwal <sudhir.mitharwal@tourepedia.com>
2019-06-22 18:59:58 +0530

    feat(button): add more states to button component

--- packages/button/examples/basic.example.tsx
+++ packages/button/examples/basic.example.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { storiesOf } from "@storybook/react"
 
-import Button, { ButtonGroup } from "./../src/index"
+import Button, { ButtonGroup, ButtonToolbar } from "./../src/index"
 
 const stories = storiesOf("Components/Button", module)
 
@@ -9,6 +9,10 @@ stories.add("Buttons", () => (
   <div>
     <Button>Default</Button>
     <Button primary>Primary</Button>
+    <Button success>Success</Button>
+    <Button error>Error</Button>
+    <Button warning>Warning</Button>
+    <Button accent>Accent</Button>
   </div>
 ))
 stories.add("Disabled Buttons", () => (
@@ -17,11 +21,40 @@ stories.add("Disabled Buttons", () => (
     <Button disabled primary>
       Primary
     </Button>
+    <Button disabled success>
+      Success
+    </Button>
+    <Button disabled error>
+      Error
+    </Button>
+    <Button disabled warning>
+      Warning
+    </Button>
+    <Button disabled accent>
+      Accent
+    </Button>
   </div>
 ))
 stories.add("Button Group", () => (
   <ButtonGroup>
     <Button>Default</Button>
     <Button primary>Primary</Button>
+    <Button success>Success</Button>
+    <Button error>Error</Button>
+    <Button warning>Warning</Button>
+    <Button accent>accent</Button>
   </ButtonGroup>
+))
+
+stories.add("Button Toolbar", () => (
+  <ButtonToolbar>
+    <Button>Default</Button>
+    <Button primary>Primary</Button>
+    <Button success>Success</Button>
+    <ButtonGroup>
+      <Button error>Error</Button>
+      <Button warning>Warning</Button>
+    </ButtonGroup>
+    <Button accent>accent</Button>
+  </ButtonToolbar>
 ))
--- packages/button/src/index.tsx
+++ packages/button/src/index.tsx
@@ -2,19 +2,42 @@ import * as React from "react"
 import { Omit } from "utility-types"
 import classNames from "classnames"
 
-type Button = Omit<React.HTMLProps<HTMLButtonElement>, "type" | "as"> & {
+export type Button = Omit<React.HTMLProps<HTMLButtonElement>, "type" | "as"> & {
   type?: "button" | "submit" | "reset"
   primary?: boolean
+  success?: boolean
+  error?: boolean
+  warning?: boolean
+  accent?: boolean
 }
 
 const Button = React.forwardRef(
   (
-    { className, primary, type = "button", ...props }: Button,
+    {
+      className,
+      primary,
+      success,
+      error,
+      warning,
+      accent,
+      type = "button",
+      ...props
+    }: Button,
     ref: React.Ref<HTMLButtonElement>
   ) => {
     return (
       <button
-        className={classNames("btn", primary && "btn-primary", className)}
+        className={classNames(
+          "btn",
+          {
+            "btn-primary": primary,
+            "btn-success": success,
+            "btn-error": error,
+            "btn-warning": warning,
+            "btn-accent": accent,
+          },
+          className
+        )}
         ref={ref}
         type={type}
         {...props}
@@ -32,4 +55,11 @@ export function ButtonGroup({
   ...props
 }: React.HTMLProps<HTMLSpanElement>) {
   return <span className={classNames("btn-group", className)} {...props} />
+}
+
+export function ButtonToolbar({
+  className,
+  ...props
+}: React.HTMLProps<HTMLSpanElement>) {
+  return <span className={classNames("btn-toolbar", className)} {...props} />
 }
