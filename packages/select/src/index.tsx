e4f57c44819bd7e7aaf5d0825d6048a6a3a7b835
Sudhir Mitharwal <sudhir.mitharwal@tourepedia.com>
2019-07-16 19:02:38 +0530

    feat(select): add keyboard navigation support
    fixes #4

--- packages/select/src/index.tsx
+++ packages/select/src/index.tsx
@@ -78,6 +78,17 @@ export function Select({
   value,
   isLoading,
 }: SelectProps) {
+  const groupRef = useRef<HTMLDivElement>(null)
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [isFocused, changeFocusState] = useState<boolean>(false)
+  useEffect(() => {
+    if (fetchOnMount) {
+      onQuery && onQuery(query || "")
+    }
+  }, [fetchOnMount])
+  const [focusedOption, changeFocusedOption] = useState<number | undefined>(
+    undefined
+  )
   const name: string = propName || (multiple ? "select[]" : "select")
   if (
     creatable &&
@@ -107,36 +118,27 @@ export function Select({
     )
     options = options.concat(moreOptions)
   }
-  const groupRef = useRef<HTMLDivElement>(null)
-  const inputRef = useRef<HTMLInputElement>(null)
-  const [isFocused, changeFocusState] = useState<boolean>(false)
-  useEffect(() => {
-    if (fetchOnMount) {
-      onQuery && onQuery(query || "")
-    }
-  }, [])
   function setIsFouced(isFocused: boolean) {
     changeFocusState(isFocused)
     if (!isFocused) {
       onBlur && onBlur({ target: { name } })
     }
   }
+  // handle the focused state
   useEffect(() => {
-    function handleClick(e: any) {
-      if (groupRef.current) {
-        const container = groupRef.current
-        if (contains(container, e.target)) {
-          if (!isFocused) {
-            setIsFouced(true)
-          }
-        } else if (isFocused) {
-          setIsFouced(false)
-        }
-      }
-    }
     const document = ownerDocument()
-    if (!document || disabled) {
+    if (!document || disabled || !groupRef.current) {
       return () => {}
+    }
+    function handleClick(e: any) {
+      const container = groupRef.current
+      if (contains(container, e.target)) {
+        if (!isFocused) {
+          setIsFouced(true)
+        }
+      } else if (isFocused) {
+        setIsFouced(false)
+      }
     }
     document.addEventListener("click", handleClick)
     document.addEventListener("keyup", handleClick)
@@ -144,8 +146,63 @@ export function Select({
       document.removeEventListener("click", handleClick)
       document.removeEventListener("keyup", handleClick)
     }
-  }, [isFocused])
-  function handleChange(option: any, checked: boolean) {
+  }, [isFocused, groupRef.current])
+  // handle the keyboad navigation
+  useEffect(() => {
+    const document = ownerDocument()
+    // handle the base cases where no need to manage the keyboad focus stuff
+    if (!isFocused || !document || disabled || !options || !options.length) {
+      changeFocusedOption(undefined)
+      return () => {}
+    }
+    // if no option is focused
+    // focus the first selected option or first option if no option is selected
+    if (focusedOption === undefined) {
+      const firstSelectedValue = Array.isArray(value) ? value[0] : value
+      if (!firstSelectedValue) {
+        changeFocusedOption(0)
+      } else {
+        const selectedOptionIndex = options.findIndex(
+          o => o.id === firstSelectedValue.id
+        )
+        changeFocusedOption(selectedOptionIndex)
+      }
+    }
+    function handleKeyDown(e: any) {
+      const { key } = e
+      const numberOfOptions = options.length
+      switch (key) {
+        case "ArrowDown":
+          changeFocusedOption(((focusedOption || 0) + 1) % numberOfOptions)
+          break
+        case "ArrowUp":
+          changeFocusedOption(
+            (() => {
+              const x = ((focusedOption || 0) - 1) % numberOfOptions
+              return x < 0 ? x + numberOfOptions : x
+            })()
+          )
+          break
+        case "Enter":
+          e.preventDefault()
+          const option = options[focusedOption || 0]
+          const checked = value
+            ? Array.isArray(value)
+              ? value.some(v => v.id === option.id)
+              : (value as any).id === option.id
+            : false
+          handleOptionClick(option, !checked)
+          break
+        default:
+          break
+      }
+    }
+    document.addEventListener("keydown", handleKeyDown)
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown)
+    }
+  }, [isFocused, focusedOption, options, value])
+  function handleOptionClick(option: any, checked: boolean) {
     const newValues = checked
       ? Array.isArray(value)
         ? value.concat([option])
@@ -190,7 +247,7 @@ export function Select({
               Type to search...
             </li>
           ) : null}
-          {options.map(option => {
+          {options.map((option, i) => {
             const checked = value
               ? Array.isArray(value)
                 ? value.some(v => v.id === option.id)
@@ -201,10 +258,11 @@ export function Select({
                 key={option.id}
                 role="option"
                 aria-selected={checked}
+                data-focused={i === focusedOption}
                 title={option.title || option.description}
                 tabIndex={-1}
                 onClick={() => {
-                  !disabled && handleChange(option, !checked)
+                  !disabled && handleOptionClick(option, !checked)
                 }}
               >
                 {option[labelKey]}
