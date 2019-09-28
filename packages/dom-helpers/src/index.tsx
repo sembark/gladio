40d72720fa8fb1667ee6ccff14cac26c6122ec14
Sudhir Mitharwal <sudhir.mitharwal@tourepedia.com>
2019-09-28 19:44:46 +0530

    feat(dom-helper): add-remove event listner utility

--- packages/dom-helpers/src/index.tsx
+++ packages/dom-helpers/src/index.tsx
@@ -55,3 +55,94 @@ export function activeElement(
   }
   return null
 }
+
+/**
+ * Add event listener
+ */
+export let optionsSupported = false
+export let onceSupported = false
+
+try {
+  const options = {
+    get passive() {
+      return (optionsSupported = true)
+    },
+    get once() {
+      // eslint-disable-next-line no-multi-assign
+      return (onceSupported = optionsSupported = true)
+    },
+  }
+  if (isDom) {
+    window.addEventListener("test", options as any, options)
+    window.removeEventListener("test", options as any, true)
+  }
+} catch (e) {
+  /* */
+}
+
+export type EventHandler<K extends keyof HTMLElementEventMap> = (
+  this: HTMLElement,
+  event: HTMLElementEventMap[K]
+) => any
+
+export type TaggedEventHandler<
+  K extends keyof HTMLElementEventMap
+> = EventHandler<K> & { __once?: EventHandler<K> }
+/**
+ * An `addEventListener` ponyfill, supports the `once` option
+ */
+export function addEventListener<K extends keyof HTMLElementEventMap>(
+  node: HTMLElement | Document,
+  eventName: K,
+  handler: TaggedEventHandler<K>,
+  options?: boolean | AddEventListenerOptions
+) {
+  if (options && typeof options !== "boolean" && !onceSupported) {
+    const { once, capture } = options
+    let wrappedHandler = handler
+    if (!onceSupported && once) {
+      wrappedHandler =
+        handler.__once ||
+        function onceHandler(event) {
+          this.removeEventListener(eventName, onceHandler, capture)
+          handler.call(this, event)
+        }
+      handler.__once = wrappedHandler
+    }
+
+    node.addEventListener(
+      eventName,
+      wrappedHandler,
+      optionsSupported ? options : capture
+    )
+  }
+
+  node.addEventListener(eventName, handler, options)
+}
+
+export function removeEventListener<K extends keyof HTMLElementEventMap>(
+  node: HTMLElement | Document,
+  eventName: K,
+  handler: TaggedEventHandler<K>,
+  options?: boolean | EventListenerOptions
+) {
+  let capture =
+    options && typeof options !== "boolean" ? options.capture : options
+
+  node.removeEventListener(eventName, handler, capture)
+  if (handler.__once) {
+    node.removeEventListener(eventName, handler.__once, capture)
+  }
+}
+
+export function listen<K extends keyof HTMLElementEventMap>(
+  node: HTMLElement | Document,
+  eventName: K,
+  handler: EventHandler<K>,
+  options?: boolean | AddEventListenerOptions
+) {
+  addEventListener(node, eventName, handler, options)
+  return () => {
+    removeEventListener(node, eventName, handler, options)
+  }
+}
