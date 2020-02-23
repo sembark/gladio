920b86d636eae2a30601b97205acc95c1c0cdc21
Sudhir Mitharwal <sudhir.mitharwal@tourepedia.com>
2020-02-23 21:23:36 +0530

    fix(snackbar): add min width to snackbar

--- packages/snackbar/src/styles.js
+++ packages/snackbar/src/styles.js
@@ -18,6 +18,10 @@ module.exports = function addBadgeComponentPlugin({ addComponents, theme }) {
       boxShadow:
         "0 3px 5px -1px rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12)",
       color: "#e2e8f0",
+      minWidth: "280px",
+      "@media (max-width: 320px)": {
+        minWidth: "unset",
+      },
       "&__label": {
         paddingLeft: "8px",
         color: "inherit !important",
